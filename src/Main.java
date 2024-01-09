8396a836b1ab5a0b4a94ea187e00b4fb34a8589b
epgokce <142578266+epgokce@users.noreply.github.com>
2024-01-09 15:53:02 +0300

    Add isPerfectNumber method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Finding Palindrome Number");
+        System.out.println("Finding Palindrome Numbers:");
         System.out.println(isPalindrome(-1221)); //true
         System.out.println(isPalindrome(707)); //true
         System.out.println(isPalindrome(11212)); //false
+        System.out.println("Finding Perfect Numbers:");
+        System.out.println(isPerfectNumber(6));   // true
+        System.out.println(isPerfectNumber(28));  // true
+        System.out.println(isPerfectNumber(5));   // false
+        System.out.println(isPerfectNumber(-1));  // false
     }
 
     public static boolean isPalindrome(int number) {
-        if (number < 0) {
-            return false; //
-        }
 
         int originalNumber = number;
         int reversedNumber = 0;
@@ -21,5 +23,16 @@ public class Main {
         }
 
         return originalNumber == reversedNumber;
+    }
+    public static boolean isPerfectNumber(int number) {
+
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
     }
 }
