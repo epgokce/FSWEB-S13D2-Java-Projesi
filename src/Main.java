4b149ac56ffee87f0e5da65b211ec19671a0f346
epgokce <142578266+epgokce@users.noreply.github.com>
2024-01-09 15:38:48 +0300

    Add isPalindrome method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Finding Palindrome Number");
+        System.out.println(isPalindrome(-1221)); //true
+        System.out.println(isPalindrome(707)); //true
+        System.out.println(isPalindrome(11212)); //false
+    }
+
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            return false; //
+        }
+
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reversedNumber = reversedNumber * 10 + lastDigit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
     }
 }
